b5f1bc56b3cb8febef3bae3179ca04a7c184efd6
gabi-p <gabi.p@dealium.ie>
2013-11-04 16:24:58 +0200

    new function

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>My Test Page</title>
     </head>
     <body>
         <?php
-        // put your code here
+        
+        function test ($a){
+            return $a;
+        }
+        
+        echo test('testing');
         
         ?>
     </body>
